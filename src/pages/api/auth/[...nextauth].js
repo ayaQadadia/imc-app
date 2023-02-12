a452936f1dc3325334aee4d9472b67c28c4bb453
Aya Qadadia <aya.qadadia@epita.fr>
2023-02-12 14:23:57 -0500

    edit env 2.0

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -26,6 +26,7 @@ export const authOptions = {
         }),
     ],
     session: { strategy: "jwt" },
-    secret: process.env.NEXTAUTH_SECRET
+    secret: process.env.NEXTAUTH_SECRET,
+    debug: true
 }
 export default NextAuth(authOptions);
